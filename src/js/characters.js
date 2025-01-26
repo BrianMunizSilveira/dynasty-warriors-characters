60db36869aa9f00c211759575eae8c45e9a8d4c9
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2025-01-25 21:08:37 -0300

    ✨ feat: novas infos

--- src/js/characters.js
+++ src/js/characters.js
@@ -1,76 +1,99 @@
-// Dados dos personagens
 export const characters = {
     wei: [
         {
             name: 'Cao Cao',
             role: 'Leader of Wei',
             weapon: 'Chaos Sword',
+            birthYear: 155,
+            deathYear: 220,
             image: './src/images/characters/caocao.webp'
         },
         {
             name: 'Cao Pi',
             role: 'Cao Cao\'s Son',
             weapon: 'Twin Sabers',
+            birthYear: 187,
+            deathYear: 226,
             image: './src/images/characters/caopi.webp'
         },
         {
             name: 'Cao Ren',
             role: 'Wei General',
             weapon: 'Shield and Sword',
+            birthYear: 168,
+            deathYear: 223,
             image: './src/images/characters/caoren.webp'
         },
         {
             name: 'Dian Wei',
             role: 'Cao Cao\'s Bodyguard',
-            weapon: '',
+            weapon: 'Battle Axe',
+            birthYear: 170,
+            deathYear: 197,
             image: './src/images/characters/dianwei.webp'
         },
         {
             name: 'Sima Yi',
             role: 'Strategist of Wei',
             weapon: 'Feather Fan',
+            birthYear: 179,
+            deathYear: 251,
             image: './src/images/characters/simayi.webp'
         },
         {
             name: 'Xiahou Dun',
             role: 'Wei General',
             weapon: 'Podao',
+            birthYear: 160,
+            deathYear: 225,
             image: './src/images/characters/xiahoudun.webp'
         },
         {
             name: 'Xiahou Yuan',
             role: 'Wei General',
             weapon: 'Thunder Bow',
+            birthYear: 165,
+            deathYear: 212,
             image: './src/images/characters/xiahouyuan.webp'
         },
         {
             name: 'Xu Huang',
             role: 'Wei General',
             weapon: 'Battle Axe',
+            birthYear: 171,
+            deathYear: 230,
             image: './src/images/characters/xuhuang.webp'
         },
         {
             name: 'Xu Zhu',
             role: 'Cao Cao\'s Personal Bodyguard',
             weapon: 'Great Club',
+            birthYear: 180,
+            deathYear: 230,
             image: './src/images/characters/xuzhu.webp'
         },
         {
             name: 'Zhang He',
             role: 'Wei Elite General',
             weapon: 'Claws',
+            birthYear: 168,
+            deathYear: 228,
             image: './src/images/characters/zhanghe.webp'
         },
         {
             name: 'Zhang Liao',
             role: 'Wei General',
             weapon: 'Halberd',
+            birthYear: 169,
+            deathYear: 222,
             image: './src/images/characters/zhangliao.webp'
         },
         {
             name: 'Zhen Ji',
             role: 'Cao Pi\'s Wife',
             weapon: 'Flute',
+            birthYear: 188,
+            deathYear: 250,
             image: './src/images/characters/zhenji.webp'
         }
     ],
@@ -79,60 +102,80 @@ export const characters = {
             name: 'Liu Bei',
             role: 'Leader of Shu',
             weapon: 'Twin Swords',
+            birthYear: 161,
+            deathYear: 223,
             image: './src/images/characters/liubei.webp'
         },
         {
             name: 'Guan Yu',
             role: 'Shu General',
             weapon: 'Green Dragon Crescent Blade',
+            birthYear: 162,
+            deathYear: 219,
             image: './src/images/characters/guanyu.webp'
         },
         {
             name: 'Zhang Fei',
             role: 'Shu General',
             weapon: 'Serpent Spear',
+            birthYear: 163,
+            deathYear: 221,
             image: './src/images/characters/zhangfei.webp'
         },
         {
             name: 'Zhao Yun',
             role: 'Shu General',
             weapon: 'Dragon Spear',
+            birthYear: 168,
+            deathYear: 229,
             image: './src/images/characters/zhaoyun.webp'
         },
         {
             name: 'Ma Chao',
             role: 'Shu General',
             weapon: 'Spear',
+            birthYear: 176,
+            deathYear: 222,
             image: './src/images/characters/machao.webp'
         },
         {
             name: 'Huang Zhong',
             role: 'Shu General',
             weapon: 'Bow',
+            birthYear: 140,
+            deathYear: 220,
             image: './src/images/characters/huangzhong.webp'
         },
         {
             name: 'Zhuge Liang',
             role: 'Principal Strategist',
             weapon: 'Feather Fan',
+            birthYear: 181,
+            deathYear: 234,
             image: './src/images/characters/zhugeliang.webp'
         },
         {
             name: 'Pang Tong',
             role: 'Shu Strategist',
             weapon: 'Staff',
+            birthYear: 179,
+            deathYear: 214,
             image: './src/images/characters/pangtong.webp'
         },
         {
             name: 'Wei Yan',
             role: 'Shu General',
             weapon: 'Double Voulge',
+            birthYear: 170,
+            deathYear: 234,
             image: './src/images/characters/weiyan.webp'
         },
         {
             name: 'Yue Ying',
             role: 'Zhuge Liang\'s Wife',
             weapon: 'Crossbow',
+            birthYear: 183,
+            deathYear: 241,
             image: './src/images/characters/yueying.webp'
         }
     ],
@@ -141,78 +184,104 @@ export const characters = {
             name: 'Sun Jian',
             role: 'Founder of Wu',
             weapon: 'Nine Rings Sword',
+            birthYear: 155,
+            deathYear: 191,
             image: './src/images/characters/sunjian.webp'
         },
         {
             name: 'Sun Ce',
             role: 'Leader of Wu',
             weapon: 'Tonfa',
+            birthYear: 175,
+            deathYear: 200,
             image: './src/images/characters/sunce.webp'
         },
         {
             name: 'Sun Quan',
             role: 'Emperor of Wu',
             weapon: 'Flame Sword',
+            birthYear: 182,
+            deathYear: 252,
             image: './src/images/characters/sunquan.webp'
         },
         {
             name: 'Sun Shangxiang',
             role: 'Political alliance',
             weapon: 'Bow',
+            birthYear: 189,
+            deathYear: 245,
             image: './src/images/characters/sunshangxiang.webp'
         },
         {
             name: 'Gan Ning',
             role: 'Wu General',
             weapon: 'River Master',
+            birthYear: 170,
+            deathYear: 220,
             image: './src/images/characters/ganning.webp'
         },
         {
             name: 'Xiao Qiao',
             role: 'Zhou Yu\'s wife',
             weapon: 'Twin Fans',
+            birthYear: 182,
+            deathYear: 230,
             image: './src/images/characters/xiaoqiao.webp'
         },
         {
             name: 'Zhou Yu',
             role: 'Commander of Wu',
             weapon: 'Ancient Sword',
+            birthYear: 175,
+            deathYear: 210,
             image: './src/images/characters/zhouyu.webp'
         },
         {
             name: 'Huang Gai',
             role: 'Veteran General',
             weapon: 'Iron Rod',
+            birthYear: 140,
+            deathYear: 219,
             image: './src/images/characters/huanggai.webp'
         },
         {
             name: 'Ling Tong',
             role: 'Wu Officer',
             weapon: 'Three Section Staff',
+            birthYear: 168,
+            deathYear: 220,
             image: './src/images/characters/lingtong.webp'
         },
         {
             name: 'Lu Meng',
             role: 'Strategic Commander',
             weapon: 'Pike',
+            birthYear: 158,
+            deathYear: 219,
             image: './src/images/characters/lumeng.webp'
         },
         {
             name: 'Lu Xun',
             role: 'Strategist',
             weapon: 'Twin Swords',
+            birthYear: 183,
+            deathYear: 245,
             image: './src/images/characters/luxun.webp'
         },
         {
             name: 'Taishi Ci',
             role: 'Wu General',
             weapon: 'Twin Rods',
+            birthYear: 165,
+            deathYear: 205,
             image: './src/images/characters/taishici.webp'
         },
         {
             name: 'Zhou Tai',
             role: 'Sun Quan\'s Bodyguard',
             weapon: 'Curved Sword',
+            birthYear: 170,
+            deathYear: 236,
             image: './src/images/characters/zhoutai.webp'
         }
     ],
@@ -221,30 +290,40 @@ export const characters = {
             name: 'Lu Bu',
             role: 'Mighty Warrior',
             weapon: 'Sky Piercer',
+            birthYear: 157,
+            deathYear: 199,
             image: './src/images/characters/lubu.webp'
         },
         {
             name: 'Dong Zhuo',
             role: 'Tyrant',
             weapon: 'Flame Blade',
+            birthYear: 150,
+            deathYear: 192,
             image: './src/images/characters/dongzhuo.webp'
         },
         {
             name: 'Diao Chan',
             role: 'Dancer',
             weapon: 'Mace',
+            birthYear: 176,
+            deathYear: 220,
             image: './src/images/characters/diaochan.webp'
         },
         {
             name: 'Yuan Shao',
             role: 'Tyrant',
             weapon: 'Spear',
+            birthYear: 154,
+            deathYear: 202,
             image: './src/images/characters/yuanshao.webp'
         },
         {
             name: 'Zhang Jiao',
             role: 'Yellow Turban Leader',
             weapon: 'Spear',
+            birthYear: 140,
+            deathYear: 184,
             image: './src/images/characters/zhangjiao.webp'
         }
     ]
--- src/js/main.js
+++ src/js/main.js
@@ -1,91 +1,138 @@
-import { characters} from './characters.js';
+import { characters } from './characters.js';
 
-// Estado da aplicação
-let currentKingdom = 'wei';
-let favorites = new Set();
-let isLoading = false;
-const audio = {
-    hover: new Audio('hover.mp3'),
-    select: new Audio('select.mp3')
-};
-
-// Funções auxiliares
-function playSound(soundName) {
-    audio[soundName].currentTime = 0;
-    audio[soundName].play().catch(() => { });
-}
-
-function createCharacterCard(char) {
-    const card = document.createElement('div');
-    card.className = 'character-card';
-    card.innerHTML = `
-      <img class="character-image" src="${char.image}" alt="${char.name}" 
-           onerror="this.src='/api/placeholder/400/500'">
-      <div class="character-info">
-        <h3 class="character-name">${char.name}</h3>
-        <p class="character-role">${char.role}</p>
-        <div class="character-stats">
-          <div class="stat">
-            <i class="fas fa-fist-raised"></i> Power: ${char.power}
-          </div>
-          <div class="stat">
-            <i class="fas fa-brain"></i> Strategy: ${char.strategy}
-          </div>
-        </div>
-      </div>
-      <div class="action-buttons">
-        <button class="action-btn favorite-btn" data-tooltip="Add to favorites">
-          <i class="fas fa-heart"></i>
-        </button>
-        <button class="action-btn info-btn" data-tooltip="More info">
-          <i class="fas fa-info"></i>
-        </button>
-      </div>
-    `;
-
-    // Event Listeners
-    card.addEventListener('mouseenter', () => playSound('hover'));
-    return card;
-}
-
-function updateCharacterGrid(kingdom) {
-    const grid = document.getElementById('charactersGrid');
-    grid.innerHTML = '';
-    isLoading = true;
-
-    // Mostrar skeleton loading
-    for (let i = 0; i < 6; i++) {
-        const skeleton = document.createElement('div');
-        skeleton.className = 'character-card skeleton';
-        grid.appendChild(skeleton);
+class CharacterManager {
+    constructor() {
+        this.currentKingdom = 'wei';
+        this.favorites = new Set();
+        this.isLoading = false;
+        this.audio = {
+            hover: new Audio('hover.mp3'),
+            select: new Audio('select.mp3')
+        };
+        this.initEventListeners();
     }
 
-    // Simular carregamento
-    setTimeout(() => {
-        grid.innerHTML = '';
-        const chars = characters[kingdom] || [];
-        chars.forEach(char => {
-            grid.appendChild(createCharacterCard(char));
+    playSound(soundName) {
+        try {
+            const sound = this.audio[soundName];
+            sound.currentTime = 0;
+            sound.play();
+        } catch (error) {
+            console.warn('Audio playback failed:', error);
+        }
+    }
+
+    createCharacterCard(char) {
+        const card = document.createElement('div');
+        card.className = 'character-card';
+        card.innerHTML = `
+            <img class="character-image" src="${char.image}" alt="${char.name}" 
+                 onerror="this.src='/api/placeholder/400/500'">
+            <div class="character-info">
+                <h3 class="character-name">${char.name}</h3>
+                <p class="character-role">${char.role}</p>
+                <div class="character-details">
+                    <div class="detail">
+                        <i class="fas fa-sword"></i> Weapon: ${char.weapon || 'Unknown'}
+                    </div>
+                    <div class="detail">
+                        <i class="fas fa-calendar-alt"></i> Born: ${char.birthYear || 'Unknown'}
+                    </div>
+                    <div class="detail">
+                        <i class="fas fa-skull"></i> Died: ${char.deathYear || 'Unknown'}
+                    </div>
+                </div>
+            </div>
+            <div class="action-buttons">
+                <button class="action-btn favorite-btn" data-tooltip="Add to favorites">
+                    <i class="fas fa-heart"></i>
+                </button>
+                <button class="action-btn info-btn" data-tooltip="More info">
+                    <i class="fas fa-info"></i>
+                </button>
+            </div>
+        `;
+
+        // Favorite button logic
+        const favoriteBtn = card.querySelector('.favorite-btn');
+        favoriteBtn.addEventListener('click', () => {
+            this.toggleFavorite(char, favoriteBtn);
         });
-        isLoading = false;
-    }, 1000);
+
+        card.addEventListener('mouseenter', () => this.playSound('hover'));
+        return card;
+    }
+
+    toggleFavorite(character, button) {
+        if (this.favorites.has(character)) {
+            this.favorites.delete(character);
+            button.classList.remove('active');
+        } else {
+            this.favorites.add(character);
+            button.classList.add('active');
+        }
+        // Optional: Persist favorites to localStorage
+        this.saveFavorites();
+    }
+
+    saveFavorites() {
+        localStorage.setItem('favorites', JSON.stringify([...this.favorites]));
+    }
+
+    loadFavorites() {
+        const savedFavorites = localStorage.getItem('favorites');
+        if (savedFavorites) {
+            this.favorites = new Set(JSON.parse(savedFavorites));
+        }
+    }
+
+    updateCharacterGrid(kingdom) {
+        const grid = document.getElementById('charactersGrid');
+        grid.innerHTML = '';
+        this.isLoading = true;
+
+        // Show skeleton loading
+        for (let i = 0; i < 6; i++) {
+            const skeleton = document.createElement('div');
+            skeleton.className = 'character-card skeleton';
+            grid.appendChild(skeleton);
+        }
+
+        // Simulate loading
+        setTimeout(() => {
+            grid.innerHTML = '';
+            const chars = characters[kingdom] || [];
+            chars.forEach(char => {
+                grid.appendChild(this.createCharacterCard(char));
+            });
+            this.isLoading = false;
+        }, 500);
+    }
+
+    initEventListeners() {
+        // Kingdom selection
+        document.querySelectorAll('.kingdom-btn').forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                document.querySelectorAll('.kingdom-btn').forEach(b => b.classList.remove('active'));
+                e.target.classList.add('active');
+                this.currentKingdom = e.target.dataset.kingdom;
+                this.updateCharacterGrid(this.currentKingdom);
+                this.playSound('select');
+            });
+        });
+
+        // Theme toggler
+        document.querySelector('[data-tooltip="Toggle Theme"]').addEventListener('click', () => {
+            document.body.dataset.theme = document.body.dataset.theme === 'light' ? 'dark' : 'light';
+        });
+    }
+
+    init() {
+        this.loadFavorites();
+        this.updateCharacterGrid('wei');
+    }
 }
 
-// Event Listeners
-document.querySelectorAll('.kingdom-btn').forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        document.querySelectorAll('.kingdom-btn').forEach(b => b.classList.remove('active'));
-        e.target.classList.add('active');
-        currentKingdom = e.target.dataset.kingdom;
-        updateCharacterGrid(currentKingdom);
-        playSound('select');
-    });
-});
-
-// Theme toggler
-document.querySelector('[data-tooltip="Toggle Theme"]').addEventListener('click', () => {
-    document.body.dataset.theme = document.body.dataset.theme === 'light' ? 'dark' : 'light';
-});
-
-// Inicialização
-updateCharacterGrid('wei');
+// Initialize the application
+const characterManager = new CharacterManager();
+characterManager.init();
